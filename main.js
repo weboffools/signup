643250a94f38185953c0d6eba27c86c7c2ff3910
weboffools <eliot@weboffools.com>
2023-11-01 09:40:44 -0600

    Add password matching functionality in main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Hello!");
+'use strict';
+
+const passwordInput = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm_pass');
+
+const passArea = document.querySelector('#password_area');
+const messageArea = document.createElement('span');
+const message = "* Passwords do not match";
+messageArea.textContent = message;
+messageArea.setAttribute('style', 'color: red');
+
+const blankMess = document.createElement('span');
+
+passwordInput.addEventListener('input', () => {
+  if (passwordInput.value != confirmPassword.value) {
+    passArea.appendChild(messageArea); 
+  }
+});
+
+confirmPassword.addEventListener('input', () => {
+  if (confirmPassword.value === passwordInput.value) {
+    passArea.replaceChild(blankMess, messageArea);
+  }
+});
+
